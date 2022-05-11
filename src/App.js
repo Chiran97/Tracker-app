1a0f80c75d8aa5802e38e7c9ea40e60221e9b799
Chiran97 <chirandilanka@gmail.com>
2022-05-11 15:08:35 +0530

    Show tasks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddTask from "./components/AddTask";
   
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
     {
@@ -37,8 +38,6 @@ const addTask = (task) =>{
   10000) + 1
   const newTask = {id, ...task}
   setTasks([...tasks, newTask])
-
-
 }
 
 
@@ -62,8 +61,12 @@ const deleteTask = (id) => {
   return (
     <div className="container">
 
-      <Header/>
-      <AddTask onAdd={addTask}/>
+      <Header 
+      onAdd={ () => setShowAddTask 
+      (!showAddTask)}
+      showAdd={showAddTask}
+      />
+      { showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length >0 ?<Tasks tasks={tasks} 
       onDelete={deleteTask} onToggle={toggleReminder}
       /> : 'No Tasks To Show' }  
